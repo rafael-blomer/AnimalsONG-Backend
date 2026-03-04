cdfe6e99c3a4f5fef35b22d37a0155b71cbcad60
Rafael <rafael.blomer@gmail.com>
2026-03-04 14:32:11 -0300

    fix:ajustando url do frontend no envio de emails

--- src/main/java/br/com/rafaelblomer/AnimalsOng_Backend/business/EmailService.java
+++ src/main/java/br/com/rafaelblomer/AnimalsOng_Backend/business/EmailService.java
@@ -57,7 +57,7 @@ public class EmailService {
 
         String recipientAddress = ong.getEmail();
         String subject = "Verificação de Cadastro";
-        String confirmationUrl = frontendUrl + "/confirmar-email?token=" + verificacaoEmail.getToken();
+        String confirmationUrl = frontendUrl + "/auth/confirmar-email?token=" + verificacaoEmail.getToken();
 
         String message = "Olá " + ong.getNome() + ",\n\n"
                 + "Obrigado por se cadastrar no AnimalsOng. Por favor, clique no link abaixo para ativar sua conta:\n\n"
@@ -72,7 +72,7 @@ public class EmailService {
 
         String recipientAddress = usuario.getEmail();
         String subject = "Alteração de Senha";
-        String confirmationUrl = frontendUrl + "/resetar-senha?token=" + token;
+        String confirmationUrl = frontendUrl + "/auth/resetar-senha?token=" + token;
 
         String message = "Olá " + usuario.getNome() + ",\n\n"
                 + "Houve um pedido para alterar sua senha. Se o pedido não foi feito por você, apenas ignore o email.\n\n"
